99eeea7c8b58c0998e37c8a235a70d4801773339
SenpaiSilver <silvermesprims@gmail.com>
2015-04-29 06:22:12 +0200

    Using exceptions for error handling.

--- TagAlong/app/api/users.py
+++ TagAlong/app/api/users.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from app import app
 from app.database import db_session, engine
@@ -9,13 +9,17 @@ import json
 @app.route("/user/<user_name>")
 @app.route("/User/<user_name>")
 def get_user(user_name):
-	usr = Users.query.filter_by(username=user_name).first()
-	#print(usr.id_users)
-	#print(usr.user_name)
-	js = {
+	try:
+		usr = Users.query.filter_by(username=user_name).first()
+		media = []
+		data = {
 			"user_id": usr.id_users,
 			"user_name": usr.username,
 			"user_email": usr.email,
-			"user_level": usr.level}
-	return (json.dumps(js, sort_keys=True, indent=2))
+			"user_level": usr.level,
+			"user_media": media,
+			"user_media_count": len(media)}
+	except AttributeError:
+		data = {}
+	return (jsonify(data))
 
