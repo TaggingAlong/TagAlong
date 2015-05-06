34ec9117b2571d32926df2885a7e126bf92a7b4b
SenpaiSilver <silvermesprims@gmail.com>
2015-05-06 14:37:50 +0200

    Wrote some dirty code for a POC. It won't scale well.

--- app/api/media.py
+++ app/api/media.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from app import app
 from app.models import db_session, engine
 from app.models.media import media, Media
+from app.models.tags import tags, Tags
 
 import json
 
@@ -10,9 +11,22 @@ import json
 @app.route("/Media/<int:media>")
 def get_media(media):
 	try:
-		#mdi = Media.query.filter_by(media=id_media).first()
-		media = []
-		data = {}
+		mdi = Media.query.filter_by(id_media=media).first()
+		tg = Tags.query.filter_by(id_media=media).all()
+		lst = []
+		for ele in tg:
+			lst.append(ele.tag)
+
+		data = {
+				"media_name": mdi.filename,
+				"media_hash": mdi.hash,
+				"media_path": "storage/%s" % (mdi.filename),
+				"media_thumb": "storage/t%s" % (mdi.filename),
+				"media_mime": "",
+				"media_tags": lst,
+				"media_width": mdi.width,
+				"media_height": mdi.height
+				}
 	except AttributeError:
 		data = {}
 	return (jsonify(data))
--- app/models/tags.py
+++ app/models/tags.py
@@ -9,10 +9,10 @@ class Tags(object):
 	def __init__(self, id_tags=None, id_media=None, tag=None):
 			self.id_tags = id_tags
 			self.id_media = id_media
-			self.tag = tag			
+			self.tag = tag
 
 	def __repr__(self):
-			return '<Tags %r>' % (self.tag)
+			return '<Tags %r #%r (%r)>' % (self.tag, self.id_tags, self.id_media)
 
 tags = Table('tags', metadata,
 	Column('id_tags', Integer, primary_key=True),
