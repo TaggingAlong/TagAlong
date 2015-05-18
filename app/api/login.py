bf0e2d11962e9937ce6895f56af88fa96f909f48
SenpaiSilver <silvermesprims@gmail.com>
2015-05-18 13:33:59 +0200

    Handling /login/ (POST).

---
+++ app/api/login.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+
+from app import app
+from app.models import db_session, engine
+from app.models.users import users, Users
+
+@app.route("/user/<user_name>")
+@app.route("/User/<user_name>")
+def get_user(user_name):
+	try:
+		usr = Users.query.filter_by(username=user_name).first()
+		media = []
+		data = {
+			"user_id": usr.id_users,
+			"user_name": usr.username,
+			"user_email": usr.email,
+			"user_media": media,
+			"user_media_count": len(media)}
+	except AttributeError:
+		data = {}
+	return (jsonify(data))
+
+@app.route("/user/id/<int:id>")
+@app.route("/User/id/<int:id>")
+def get_user_id(id):
+	try:
+		usr = Users.query.filter_by(id_users=id).first()
+		media = []
+		data = {
+			"user_id": usr.id_users,
+			"user_name": usr.username,
+			"user_email": usr.email,
+			"user_media": media,
+			"user_media_count": len(media)}
+	except AttributeError:
+		data = {}
+	return (jsonify(data))
